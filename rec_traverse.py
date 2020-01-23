dde82b767461a88261709030cfee6bc2353ba0da
Xavier Vani-Charron <xavier.vani@gmail.com>
2020-01-23 11:44:38 -0500

    Directories can be renamed

--- rec_traverse.py
+++ rec_traverse.py
@@ -1,39 +1,42 @@
 import os
- 
-rootDir = '.'
+
+def check_chars(dirName):
+    if '\"' in dirName or '*' in dirName or ':' in dirName or '<' in dirName or '>' in dirName or '?' in dirName or '/' in dirName or '\\' in dirName or '|' in dirName:
+        return True
+    else:
+        return False
+
+def replace_chars(fullpath):
+    
+    for i in fullpath:
+        if i == '\"':
+            fullpath = fullpath.replace('\"', ' in')
+        elif i == '*':
+            fullpath = fullpath.replace('*', ' ')
+        elif i == ':':
+            fullpath = fullpath.replace(':', ' ')
+        elif i == '<':
+            fullpath = fullpath.replace('<', ' ')
+        elif i == '>':
+            fullpath = fullpath.replace('>', ' ')
+        elif i == '?':
+            fullpath = fullpath.replace('?', ' ')
+        # elif i == '/':
+        #     fullpath = fullpath.replace('/', ' ')
+        elif i == '\\':
+            fullpath = fullpath.replace('\\', ' ')
+        elif i == '|':
+            fullpath = fullpath.replace('|', 'test')
+    return fullpath
+
+
+
+rootDir = './test'
+for dirName, subdirList, fileList in os.walk(rootDir):
+    if check_chars(dirName):
+        os.replace(dirName, replace_chars(dirName))
 
 for dirName, subdirList, fileList in os.walk(rootDir):
-
-    print(f'Found directory: {dirName}')
-
-    check_for_chars(dirName)
-
-    for fname in fileList:
-        print(f'\t{fname}')
-
-
-def replace_chars(fileName):
-    for i in fileName:
-        if i == '\"':
-            fileName = fileName.replace('\"', ' in')
-        elif i == '*':
-            fileName = fileName.replace('*', ' ')
-        elif i == ':':
-            fileName = fileName.replace(':', ' ')
-        elif i == '<':
-            fileName = fileName.replace('<', ' ')
-        elif i == '>':
-            fileName = fileName.replace('>', ' ')
-        elif i == '?':
-            fileName = fileName.replace('?', ' ')
-        elif i == '/':
-            fileName = fileName.replace('/', ' ')
-        elif i == '\\':
-            fileName = fileName.replace('\\', ' ')
-        elif i == '|':
-            fileName = fileName.replace('|', ' ')
-    return fileName
-
-def check_for_chars(fullpath):
-    if '\"' in fullpath or '*' in fullpath or ':' in fullpath or '<' in fullpath or '>' in fullpath or '?' in fullpath or '/' in fullpath or '\\' in fullpath or '|' in fullpath:
-        return replace_chars(fullpath)
+    for fileName in fileList:
+        if check_chars(fileName):
+            os.replace(fileName, replace_chars(fileName))

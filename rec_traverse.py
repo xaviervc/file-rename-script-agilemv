d75d23a2d4f952051811a0f0bedf1b21df98e301
Xavier Vani-Charron <xavier.vani@gmail.com>
2020-01-27 09:36:29 -0500

    Script complete and tested

--- rec_traverse.py
+++ rec_traverse.py
@@ -1,13 +1,15 @@
 import os
+import logging
 
 def check_chars(dirName):
-    if '\"' in dirName or '*' in dirName or ':' in dirName or '<' in dirName or '>' in dirName or '?' in dirName or '/' in dirName or '\\' in dirName or '|' in dirName:
+    # Removed '/' in dirName or from the list as it is causing false positives due to the Linux file system
+    if '\"' in dirName or '*' in dirName or ':' in dirName or '<' in dirName or '>' in dirName or '?' in dirName or '\\' in dirName or '|' in dirName:
         return True
     else:
         return False
 
 def replace_chars(fullpath):
-    
+    # Commented out '/' due to filepath false positive on linux
     for i in fullpath:
         if i == '\"':
             fullpath = fullpath.replace('\"', ' in')
@@ -26,17 +28,23 @@ def replace_chars(fullpath):
         elif i == '\\':
             fullpath = fullpath.replace('\\', ' ')
         elif i == '|':
-            fullpath = fullpath.replace('|', 'test')
+            fullpath = fullpath.replace('|', ' ')
     return fullpath
 
-
+logging.basicConfig(filename='char_replacement.log', level=logging.DEBUG)
 
 rootDir = './test'
 for dirName, subdirList, fileList in os.walk(rootDir):
     if check_chars(dirName):
-        os.replace(dirName, replace_chars(dirName))
+        logging.info(f'Illegal Character Found at Directory {dirName}')
+        replacement_string = replace_chars(dirName)
+        logging.info(f'Replaced by {replacement_string}')
+        os.replace(dirName, replace_chars(replacement_string))
 
 for dirName, subdirList, fileList in os.walk(rootDir):
     for fileName in fileList:
         if check_chars(fileName):
-            os.replace(fileName, replace_chars(fileName))
+            logging.info(f'Illegal Character Found at Filename {fileName}')
+            replacement_string = replace_chars(dirName + '/' + fileName)
+            logging.info(f'Replaced by {replacement_string}')
+            os.replace(dirName + '/' + fileName, replacement_string)
